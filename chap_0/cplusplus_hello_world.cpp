b8fa7d34823f24e7215d6175cf38071fac202f17
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2020-08-23 22:18:00 +0545

    chap0: sample c++ hello world program

---
+++ chap_0/cplusplus_hello_world.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+/*
+    Hello World program written in C++
+    Note the extension of c++ (*.cpp)
+
+    author: ranjan-stha
+*/
+
+using namespace std;
+
+int main(){
+    cout << "Hello Class of First Year Electronics ACEM" << endl;
+    
+    return 0;
+}
