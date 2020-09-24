3d809af733b35208949a34c6206c354767393046
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2020-09-24 08:09:33 +0545

    chap4: updated friend class;

--- chap_4/friend_class.cpp
+++ chap_4/friend_class.cpp
@@ -14,22 +14,30 @@ class Employee {
         int eid;
         float salary;
         float tax;
+        float tax_res;
+        void calc_tax(){
+            tax = 0.15 * salary;
+        }
+        void calc_resource_tax(){
+            tax_res = 0.01 * salary;
+        }
     public:
         Employee(int id, float sal) {
             eid = id;
             salary = sal;
         };
         friend class Finance; // Class Finance is a friend of Class Employee
-        void calc_tax(){
-            tax = 0.15 * salary;
-        }
+        
 };
 
 class Finance {
     public:
         void check_emp_details(Employee e){
             e.calc_tax();
-            cout << "Net Salary is " << e.salary - e.tax << endl;
+            e.calc_resource_tax();
+            cout << "Tax is " << e.tax << endl;
+            cout << "Resource tax is " << e.tax_res << endl;
+            cout << "Net Salary is " << e.salary - e.tax - e.tax_res << endl;
         }
 };
 
